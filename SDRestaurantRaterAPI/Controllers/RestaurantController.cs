b0d430f8e5514239c91bd9b534c29e5d3b495542
theMagnificentJay <reedjoshua408@gmail.com>
2021-05-25 12:11:12 -0500

    Controller

---
+++ SDRestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,33 @@
+﻿using SDRestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SDRestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ SDRestaurantRaterAPI/Models/RestaurantDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SDRestaurantRaterAPI.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext() : base("DefaultConnection") { }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
