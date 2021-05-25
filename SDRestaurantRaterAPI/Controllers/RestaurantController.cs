7aedb4ef5dd5a5f34cf02d5fde001e6fb2a44ee2
theMagnificentJay <reedjoshua408@gmail.com>
2021-05-25 14:01:12 -0500

    Added Update and Delete to restaurant controller

--- SDRestaurantRaterAPI/Controllers/RestaurantController.cs
+++ SDRestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -54,7 +54,46 @@ namespace SDRestaurantRaterAPI.Controllers
         }
 
         //Update(PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant(int id, Restaurant updatedRestaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+                if(restaurant != null)
+                {
+                    restaurant.Name = updatedRestaurant.Name;
+                    restaurant.Address = updatedRestaurant.Address;
+                    restaurant.Rating = updatedRestaurant.Rating;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
 
         //Delete
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+             
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The restaurant was successfully deleted.");
+            }
+
+            return InternalServerError();
+        }
     }
 }
