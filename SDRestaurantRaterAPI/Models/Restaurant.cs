deb3d1d8e61c9aafcef3387c196b9ac80cb43b45
theMagnificentJay <reedjoshua408@gmail.com>
2021-05-25 08:55:29 -0500

    class props

--- SDRestaurantRaterAPI/Models/Restaurant.cs
+++ SDRestaurantRaterAPI/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,18 @@ namespace SDRestaurantRaterAPI.Models
 {
     public class Restaurant
     {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
+        [Required]
         public double Rating { get; set; }
-        public bool IsRecommended
-        {
-            get
-            {
-                return Rating > 3.5;
-            }
-        }
+
+        public bool IsRecommended => Rating > 3.5;
     }
 }
